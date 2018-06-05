ed020e257319e46a9ca8f8e22ed95064840a7813
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-05 19:32:54 -0400

    Added submit button functionality and changed the background and font color

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const button = document.querySelector('button')
-const b2 = document.getElementById('submit')
+const form = document.querySelector('form')
 
 function changetxt() {
-    // const h = document.querySelector('h1')
-    // h.textContent = '~Kapow~'
-
     const h2 = document.getElementById('heading2')
     h2.textContent = 'Boom. ~magic~'
 }
 
-function updateHeading() {
+const updateHeading = function(ev) {
+    ev.preventDefault()
     var newH = document.getElementById('response').value
     const h3 = document.getElementById('newHeading')
     h3.innerHTML = newH
 }
 
 button.addEventListener('click', changetxt)
-// document.querySelector('form').addEventListener('submit', updateHeading)
-b2.addEventListener('click', updateHeading)
-
+window.addEventListener('click', updateHeading)
 window.addEventListener('keyup', function(ev){
-    if(ev.keyCode === 13){
+    if(ev.keyCode === 13){  
       updateHeading();
     }
 });
+
+//const b2 = document.getElementById('submit') line2
+    // const h = document.querySelector('h1') line5
+      // h.textContent = '~Kapow~' line6
+// document.querySelector('form').addEventListener('submit', updateHeading) line17
+
