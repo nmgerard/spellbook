e651316072093942ce6a8ce2d9518d2aff2f8772
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-05 12:43:14 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('testing')
+const button = document.querySelector('button')
+const b2 = document.getElementById('submit')
+
+function changetxt() {
+    // const h = document.querySelector('h1')
+    // h.textContent = '~Kapow~'
+
+    const h2 = document.getElementById('heading2')
+    h2.textContent = 'Boom. ~magic~'
+}
+
+function updateHeading() {
+    var newH = document.getElementById('response').value
+    const h3 = document.getElementById('newHeading')
+    h3.innerHTML = newH
+}
+
+button.addEventListener('click', changetxt)
+// document.querySelector('form').addEventListener('submit', updateHeading)
+b2.addEventListener('click', updateHeading)
+
+window.addEventListener('keyup', function(ev){
+    if(ev.keyCode === 13){
+      updateHeading();
+    }
+});
