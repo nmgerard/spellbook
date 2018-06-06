b39ec2bdc1e182caa2bd9d4a54f0122c6f1e7319
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-06 09:57:16 -0400

    Fixed remaining bugs regarding the radio buttons and the text field

--- index.js
+++ index.js
@@ -13,17 +13,26 @@ const updateHeading = function(ev) {
 
     const item = document.getElementById('response').value
     document.getElementById('newHeading').innerHTML += '<p>' + item + '</p>' 
-   
-    const item2 = document.querySelector('input[name = "herb"]:checked').value
-    document.getElementById('newHeading').innerHTML += '<p>' + item2 + '</p>'
-
     form.reset()
+
+    // const item2 = document.querySelector('input[name = "herb"]:checked').value
+    // document.getElementById('newHeading').innerHTML += '<p>' + item2 + '</p>'
+    // form.reset()
+
     // var newH = document.getElementById('response').value
     // const h3 = document.getElementById('newHeading')
     // h3.innerHTML = newH
 }
 
+const clickRadioBtn = function(ev){
+    ev.preventDefault()
+    const item2 = document.querySelector('input[name = "herb"]:checked').value
+    document.getElementById('newHeading').innerHTML += '<p>' + item2 + '</p>'
+    form.reset()
+}
+
 button.addEventListener('click', changetxt)
+window.addEventListener('click', clickRadioBtn)
 window.addEventListener('click', updateHeading)
 window.addEventListener('keyup', function(ev){
     if(ev.keyCode === 13){  
