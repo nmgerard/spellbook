91732e2bdf9689eda5231a76fb0d5f28fc0140dc
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-07 00:21:45 -0400

    Change background, color and layout, with new entries now getting added to one line. Also includes an array that stores all entries

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
 const button = document.querySelector('button')
 const form = document.querySelector('form')
+const spellArray = [];
 
-function changetxt() {
-    const h2 = document.getElementById('heading2')
-    h2.textContent = 'Boom. ~magic~'
+// function changetxt() {
+//     const h2 = document.getElementById('heading2')
+//     h2.textContent = 'Boom. ~magic~'
+// }
+
+
+const addEntry = function(ev){
+    ev.preventDefault()
+
+    const spellName = document.getElementById('response').value
+    const spellTxt = document.createTextNode(spellName)
+
+    const herb = document.querySelector('input[name = "herb"]:checked').value
+    const herbTxt = document.createTextNode(herb)
+
+    const item = document.createElement('p')
+     item.appendChild(spellTxt)
+     item.appendChild(herbTxt)
+     const list = document.getElementById('newHeading')
+     list.appendChild(item)
+
+     form.reset()
 }
 
-const updateHeading = function(ev) {
-    ev.preventDefault()
-    const item = document.getElementById('response').value
-    // document.getElementById('newHeading').innerHTML += '<p>' + item + '</p>' 
+// const updateHeading = function(ev) {
+//     ev.preventDefault()
+//     const item = document.getElementById('response').value
+//     // document.getElementById('newHeading').innerHTML += '<p>' + item + '</p>' 
 
-    var node = document.createElement("p");
-    var textNode = document.createTextNode(item)
-    node.appendChild(textNode);
-    document.getElementById('newHeading').appendChild(node)
-    form.reset()
+//     var node = document.createElement("p");
+//     var textNode = document.createTextNode(item)
+//     node.appendChild(textNode);
+//     spellArray.push(node.innerHTML)
+//     document.getElementById('newHeading').appendChild(node)
+
+//     form.reset()
    
-}
+// }
 
-const clickRadioBtn = function(ev){
-    ev.preventDefault()
-    const item2 = document.querySelector('input[name = "herb"]:checked').value
-    // document.getElementById('newHeading').innerHTML += '<p>' + item2 + '</p>'
+// const clickRadioBtn = function(ev){
+//     ev.preventDefault()
+//     const item2 = document.querySelector('input[name = "herb"]:checked').value
+//     // document.getElementById('newHeading').innerHTML += '<p>' + item2 + '</p>'
 
-    var node = document.createElement("p");
-    var textNode = document.createTextNode(item2)
-    node.appendChild(textNode);
-    document.getElementById('radioHeading').appendChild(node)  
-    form.reset()
-}
+//     var node = document.createElement("p");
+//     var textNode = document.createTextNode(item2)
+//     node.appendChild(textNode);
+//     spellArray.push(node.innerHTML)
+//     document.getElementById('radioHeading').appendChild(node)  
+//     form.reset()
+// }
 
-button.addEventListener('click', changetxt)
-window.addEventListener('click', clickRadioBtn)
-window.addEventListener('click', updateHeading)
+// button.addEventListener('click', changetxt)
+
+// window.addEventListener('click', clickRadioBtn)
+// window.addEventListener('click', updateHeading)
+// window.addEventListener('keyup', function(ev){
+//     if(ev.keyCode === 13){  
+//       updateHeading();
+//     }
+// });
+
+window.addEventListener('click', addEntry)
+window.addEventListener('click', addEntry)
 window.addEventListener('keyup', function(ev){
     if(ev.keyCode === 13){  
-      updateHeading();
+      addEntry();
     }
 });
-
